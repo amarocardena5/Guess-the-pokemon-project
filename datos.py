7736e8e926c5d1e1c5bd3c061aa149b6f5ac94fe
Amaro Cardenas <amarocardenasmant@gmail.com>
2026-05-09 22:00:55 -0400

    agrego dato de 5 pokemons

---
+++ datos.py
@@ -0,0 +1,10 @@
+pokemones = [ 
+    {"nombre":"Charmander", "tipo":["Fuego"],
+      "descripcion":"Lagartija pequeña anaranjada con una llama en la punta de su cola", "evoluciones":"Charmeleon,"
+      " Charizard", "es evolucion de":"No tiene pre-evolución",
+        "habitat": "zonas montañosas y volcanicas", " evolucion_otra_generacion":"No tiene evoluciones en otras generaciones"}
+    {"nombre": "Bulbasaur", "tipo":["Planta", "Veneno"], "descripcion":"Pokemon pequeño con una semilla en su espalda que crece a medida que evoluciona"
+         , "evoluciones": "Ivysaur, Venusaur", "es evolucion de": " No tiene pre-evolución", "habitat": "bosques y praderas", "evolucion_otra_generacion": "no tiene evoluciones en otras generaciones"}
+    {"nombre":"Squirtle", "tipo":["Agua"], "descripcion":"Pokemon pequeño con caparazón azul y cola rizada", "evoluciones": "Wartortle, Blastoise", "es evolucion de": "No tiene pre-evolucion", "habitat": "cuerpose de aguea como lagos y rios", "evolucione_otra_generacion": "no tiene evolciones en otras generaciones"}
+    {"nombre":"Pikachu", "tipo":["Electrico"], "descripcion":"Pokemon pequeño amarillo con mejillas rojas que puede generar electricidad", "evoluciones":"Raichu", "es evolucion de" : "Tiene pre-evolucion en otra generación", "habitat": "bosques y zonas urbanas", "evolucion_otra_generacion": "Tiene una pre-evolución en otra generación"}
+    {"nombre":"Mewtwo", "tipo":["Psíquico"], "descripcion":"Pokemon legendario con la capacidad de manipular la mente", "evoluciones": "No tiene evoluciones", "es evolucion de": "No tiene pre-evolución", "habitat": "zonas místicas", "evolucion_otra_generacion": "No tiene evoluciones en otras generaciones"}
